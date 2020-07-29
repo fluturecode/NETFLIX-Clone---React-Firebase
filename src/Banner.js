6a8ab1c0fb1790b73bd593a0f199ff6b9ff4c951
fluturecode <fluturecode@gmail.com>
2020-07-28 21:09:27 -0400

    updated version

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import requests from "./requests";
+import "./Banner.css";
 
 function Banner() {
   const [movie, setMovie] = useState([]);
@@ -21,6 +22,10 @@ function Banner() {
 
   console.log(movie);
 
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
   return (
     <header
       className="banner"
@@ -32,13 +37,17 @@ function Banner() {
         backgroundPosition: "center center",
       }}
     >
-      <div className="banner_contents">
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
-        <div className="banner_buttons">
-          <button className="banner_button">Play</button>
-          <button className="banner_button">My List</button>
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
         </div>
-        <h1 className="banner_description">{movie?.overview}</h1>
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
       </div>
     </header>
   );
