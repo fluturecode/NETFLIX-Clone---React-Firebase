312226bb06a26762ba56a1c99f0f332812774547
fluturecode <fluturecode@gmail.com>
2020-09-12 21:55:48 -0400

    updated api

--- src/requests.js
+++ src/requests.js
@@ -1,14 +1,14 @@
-const TMDB_API_KEY = "ab3f975c938b072b5ecd12176892ba3b";
+const fun = "ab3f975c938b072b5ecd12176892ba3b";
 
 const requests = {
-	fetchTrending: `/trending/all/week?api_key=${TMDB_API_KEY}&language=en-US`,
-	fetchNetflixOriginals: `/discover/tv?api_key=${TMDB_API_KEY}&with_networks=213`,
-	fetchTopRated: `/movie/top_rated?api_key=${TMDB_API_KEY}&language=en-US`,
-	fetchActionMovies: `/discover/movie?api_key=${TMDB_API_KEY}&with_genres=28`,
-	fetchComedyMovies: `/discover/movie?api_key=${TMDB_API_KEY}&with_genres=35`,
-	fetchHorrorMovies: `/discover/movie?api_key=${TMDB_API_KEY}&with_genres=27`,
-	fetchRomanceMovies: `/discover/movie?api_key=${TMDB_API_KEY}&with_genres=10749`,
-	fetchDocumentaries: `/discover/movie?api_key=${TMDB_API_KEY}&with_genres=99`,
+	fetchTrending: `/trending/all/week?api_key=${fun}&language=en-US`,
+	fetchNetflixOriginals: `/discover/tv?api_key=${fun}&with_networks=213`,
+	fetchTopRated: `/movie/top_rated?api_key=${fun}&language=en-US`,
+	fetchActionMovies: `/discover/movie?api_key=${fun}&with_genres=28`,
+	fetchComedyMovies: `/discover/movie?api_key=${fun}&with_genres=35`,
+	fetchHorrorMovies: `/discover/movie?api_key=${fun}&with_genres=27`,
+	fetchRomanceMovies: `/discover/movie?api_key=${fun}&with_genres=10749`,
+	fetchDocumentaries: `/discover/movie?api_key=${fun}&with_genres=99`,
 };
 
 export default requests;
